fedcf4a564e18304af5fbaa924dd8df7f781f51d
jatinssh <108983194+jatinssh@users.noreply.github.com>
2022-12-31 18:34:21 +0530

    Delete Leetcode.java

--- Leetcode.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int[] shuffle(int[] nums, int n) {
-        for(int i=0;i<nums.length;i++){
-            for(int j=0;j<nums[i].length;j++){
-                nums[i]= nums[i] + nums[j];
-            }
-        }
-    }
-}
